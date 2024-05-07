07bfdf69c445e97999bcd36031b70d9ebb71ef50
kweku-annan <kwekuannan.dev@gmail.com>
2024-05-07 04:05:13 +0000

    0-create_a_file.pp puppet lint adhered

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Creates a file in /tmp.
 
 file { '/tmp/school':
-   ensure => present,
-  path => '/tmp/school',
-  owner => 'www-data',
+  ensure  => present,
+  path    => '/tmp/school',
+  owner   => 'www-data',
   content => 'I love Puppet',
-  group => 'www-data',
-  mode => '0744',
+  group   => 'www-data',
+  mode    => '0744',
 }
