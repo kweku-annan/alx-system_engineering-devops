3782ff937d2c1e2b70e9f3e65fd2f36845d52c3d
kweku-annan <kwekuannan.dev@gmail.com>
2024-05-07 03:26:12 +0000

    Add 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Creates a file in /tmp.
+
+file { '/tmp/school':
+   ensure => present,
+  path => '/tmp/school',
+  owner => 'www-data',
+  content => 'I love Puppet',
+  group => 'www-data',
+  mode => '0744',
+}
