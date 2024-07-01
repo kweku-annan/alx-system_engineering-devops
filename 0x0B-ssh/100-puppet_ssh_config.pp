e8d064db4efa15a4a37dd2109ac3714c302d84e8
kweku-annan <kwekuannan.dev@gmail.com>
2024-07-01 06:01:10 +0000

    New changes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,10 @@
 # Puppet script to create ssh config file
-file_line { 'Turn off the password authentification':
-  ensure => 'present',
+file_line { 'Turn off password authentification':
   path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+  line   => 'PasswordAuthentication no',
 }
 
 file_line { 'the identity file':
-  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school',
+  line   => 'IdentityFile ~/.ssh/school',
 }
