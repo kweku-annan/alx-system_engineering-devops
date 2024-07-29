a622852cf7cd507b665a602bfa03641768342d9c
kweku-annan <kwekuannan.dev@gmail.com>
2024-07-29 16:57:52 +0000

    Add 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Accepts an employee ID and returns information"""
+import json
 import requests
 import sys
 
@@ -10,17 +11,20 @@ if __name__ == "__main__":
     name = user.json().get('name')
 
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    print(todos.json())
-    total_tasks = 0
-    completed_tasks = 0
-    task_title = ''
+    task_data_list = []
+    data = {}
 
     for task in todos.json():
+        task_data = {}
         if task.get('userId') == int(userId):
-            total_tasks += 1
-            if task.get('completed'):
-                completed_tasks += 1
-                task_title += "\t {}\n".format(task.get('title'))
-    print('Employee {} is done with tasks({}/{}):'.format(
-        name, completed_tasks, total_tasks))
-    print(task_title, end="")
+            task_data["task"] = task.get('title')
+            task_data["completed"] = task.get('completed')
+            task_data["username"] = name
+            task_data_list.append(task_data)
+
+    data[userId] = task_data_list
+
+    file_path = f'{userId}.json'
+
+    with open(file_path, 'w') as a_file:
+        json.dump(data, a_file)
